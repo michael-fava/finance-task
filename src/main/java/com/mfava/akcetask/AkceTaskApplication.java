7e33196cc27526493912164eb01bf3172aba0ddc
Michael Fava <michael.fava@melitaltd.com>
2019-03-19 09:59:05 +0100

    Intial setup for project

---
+++ src/main/java/com/mfava/akcetask/AkceTaskApplication.java
@@ -0,0 +1,13 @@
+package com.mfava.akcetask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkceTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AkceTaskApplication.class, args);
+    }
+
+}
